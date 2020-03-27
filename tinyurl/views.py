3bf90ae0875983c17ff5226bd6d4483336e97342
GrayAn <greyan@gmail.com>
2020-03-27 10:52:16 +0300

    Fixed: updating records on short URL collision

--- tinyurl/views.py
+++ tinyurl/views.py
@@ -8,18 +8,26 @@ from services import generate_short_url
 def index():
     if request.method == 'GET':
         return render_template('request.html')
-    url_length = current_app.config['TINYURL_URLLENGTH']
-    url_association = UrlAssociation()
-    url_association.fullurl = request.form['url']
-    url_association.shorturl = generate_short_url(request.form['url'], url_length)
-    db.session.add(url_association)
-    db.session.commit()
+
+    fullurl = request.form['url']
+    shorturl = generate_short_url(fullurl, current_app.config['TINYURL_URLLENGTH'])
+
+    url_association = db.session.query(UrlAssociation).filter_by(shorturl=shorturl).first()
+    if url_association is None:
+        url_association = UrlAssociation()
+        url_association.fullurl = fullurl
+        url_association.shorturl = shorturl
+        db.session.add(url_association)
+        db.session.commit()
+    else:
+        url_association.fullurl = fullurl
+        db.session.commit()
+
     return render_template('response.html', url=url_association.shorturl)
 
 
 def shorturl(url):
-    url_association = db.session.query(UrlAssociation). \
-            filter_by(shorturl=url).first()
+    url_association = db.session.query(UrlAssociation).filter_by(shorturl=url).first()
     if url_association is None:
         abort(404)
     return redirect(url_association.fullurl)
